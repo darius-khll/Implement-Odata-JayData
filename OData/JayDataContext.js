b78ca647260dd0f75a99028d6ad3c8d5a6c284ff
Ali Khalili <ali.khll.1994@gmail.com>
2016-10-02 12:13:33 +0330

    index page modified

--- OData/JayDataContext.js
+++
@@ -1,8 +0,0 @@
-
-var Product = $data.Entity.extend("Product", {
-    Id: { type: "int", key: true, computed: true },
-    Task: { type: String, required: true, maxLength: 200 },
-    DueDate: { type: Date },
-    Completed: { type: Boolean }
-});
-
